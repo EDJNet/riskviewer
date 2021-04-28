457aa0f2bad3ac2cc600ce83eabe0b139e056885
Giorgio Comai <g@giorgiocomai.eu>
2021-04-28 14:27:15 +0200

    customise shiny app

--- R/rv_explorer_app_ui.R
+++ R/rv_explorer_app_ui.R
@@ -8,19 +8,31 @@ rv_explorer_app_ui <- function(request) {
   tagList(
     
     golem_add_external_resources(),
-    
-    fluidPage(shiny::titlePanel(title = tags$a("Riskviewer explorer - A tool by EDJNet", href='https://github.com/EDJNet/riskviewer'),
-                                          windowTitle = "Riskviewer explorer - A tool by EDJNet"),
+    tags$style("h3 a {color: #6f2c91;}"),
+    tags$style("p a {color: #6f2c91;}"),
+    tags$style("h3 a:hover {color: #a6ce39;}"),
+    tags$style("p a:hover {color: #a6ce39;}"),
+    fluidPage(
+      theme = bslib::bs_theme(
+        version = 4,
+        bootswatch = "journal",
+        bg = "white",
+        fg = "black",
+        primary = "#2780E3"
+      ),
       shiny::sidebarLayout(sidebarPanel = sidebarPanel(
         mod_rv_create_airplane_compact_ui("rv_create_compact_airplane_1"),
         shiny::hr(),
-        shiny::h4(tags$a("Find out more about this approach for visualising risk", href='https://edjnet.github.io/riskviewer/articles/introduction.html')),
+        shiny::h3(tags$a("A tool by EDJNet", href='https://github.com/edjnet/riskviewer/')),
+        shiny::h3(tags$a(tags$img(src = "www/img/edjnet_logo_full.svg"),
+                         href='https://www.europeandatajournalism.eu/')),
+        shiny::p(tags$a("Find out more about this approach for visualising risk",
+                        href='https://edjnet.github.io/riskviewer/articles/introduction.html')),
         width = 2),
-      mainPanel = mainPanel({
-        
-        plotOutput(outputId = "airplane_gg", height = 1024)
-        
-      }))
+        mainPanel = mainPanel(
+          plotOutput(outputId = "airplane_gg", height = 1024)
+          
+        ))
     )
   )
 }
@@ -38,12 +50,12 @@ golem_add_external_resources <- function(){
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+  
   tags$head(
     favicon(),
     bundle_resources(
       path = app_sys('app/www'),
-      app_title = 'riskviewer'
+      app_title = 'Riskviewer explorer - A tool by EDJNet'
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert() 
